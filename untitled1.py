12eaa7b537e84586b40075f9428da043a469c2a6
Abhinay143 <54069527+Abhinay143@users.noreply.github.com>
2020-09-03 12:05:19 +0400

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -10,9 +10,9 @@ Created on Wed Sep  2 13:00:48 2020
 import glassdoor_scraped as gs
 import pandas as pd
 path=(r"C:\Users\ABhI\Documents\salary_proj\chromedriver")
-df=gs.get_jobs("Data Scientist",10,False,path,15)
+df=gs.get_jobs("Data Scientist",1000,False,path,15)
 
-df
+df.to_csv("glassdoor_jobs.csv",index=False)
 
 
 
